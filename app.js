7de9f8d2f2c1041e405fdf0a7f9fb2524825658c
kaanklky <kaanklky@gmail.com>
2017-11-30 07:46:15 +0300

    #1, #2 ,#3 fixed

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/register.html');
 });
 
-// Chat
+// Chat page block
 app.get('/chat', function(req, res){
-  res.sendFile(__dirname + '/chat.html');
+  res.sendFile(__dirname + '/register.html');
+});
+
+// Chat to room
+app.get('/chat/((?:[a-z0-9]*))', function(req, res){
+    res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', function(socket){
@@ -23,17 +28,6 @@ io.on('connection', function(socket){
 
         // Broadcast it to everyone in the same room as the client
         io.in(data.room).emit('chat message', data);
-
-        // If the message starts with "@", the user wants to tag someone
-        if(data.message.indexOf("@") > -1) {
-            var tagged = data.message.match(/@\w+/g);
-            tagged[0] = tagged[0].substr(1);
-            data2 = {
-                tagged : tagged[0],
-                tagger : data.name
-            }
-            io.in(data.room).emit('tagged', data2);
-        }
     });
 
     // When a new user joins, we add them to their desired room
---
+++ public/js/web.js
@@ -0,0 +1,15 @@
+function makeId(length, alpha) {
+  var text = "";
+  var possible = "";
+
+  if (alpha) {
+    possible = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+  }
+
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
