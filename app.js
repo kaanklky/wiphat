7c6a03746b8dff703cd3452b02018cc917ff8771
kaanklky <kaanklky@gmail.com>
2017-12-02 02:01:09 +0300

    #9 - fix smaller hours than 10

--- app.js
+++ app.js
@@ -1,62 +1,65 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path')
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var path = require("path");
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Index
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/register.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/register.html");
 });
 
 // Chat page block
-app.get('/chat', function(req, res){
-  res.sendFile(__dirname + '/register.html');
+app.get("/chat", function(req, res) {
+  res.sendFile(__dirname + "/register.html");
 });
 
 // Chat to room
-app.get('/chat/((?:[a-z0-9]*))', function(req, res){
-    res.sendFile(__dirname + '/chat.html');
+app.get("/chat/((?:[a-z0-9]*))", function(req, res) {
+  res.sendFile(__dirname + "/chat.html");
 });
 
-io.on('connection', function(socket){
+io.on("connection", function(socket) {
+  // When a client sends a message
+  socket.on("chat message", function(data) {
+    // To add timestamp to message
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
 
-    // When a client sends a message
-    socket.on('chat message', function(data){
-        // To add timestamp to message
-        var d = new Date();
-        var h = d.getHours();
-        var m = d.getMinutes();
+    if (h < 10) {
+      h = "0" + h;
+    }
 
-        // Broadcast it to everyone in the same room as the client
-        io.in(data.room).emit('chat message', data);
-        console.log('[' + data.room + '] ' + data.name + ' (' + h + '.' + m + '): ' + data.message);
-    });
+    // Broadcast it to everyone in the same room as the client
+    io.in(data.room).emit("chat message", data);
+    console.log( "[" + data.room + "] " + data.name + " (" + h + "." + m + "): " + data.message);
+  });
 
-    // When a new user joins, we add them to their desired room
-    socket.on('new user', function(data){
-        var name = data.name;
-        var room = data.room;
-        socket.join(room);
-        data2 = {
-            name : "server",
-            room : room,
-            message : '"' + name + '"' + ' joined the room.'
-        };
+  // When a new user joins, we add them to their desired room
+  socket.on("new user", function(data) {
+    var name = data.name;
+    var room = data.room;
+    socket.join(room);
+    data2 = {
+      name: "server",
+      room: room,
+      message: '"' + name + '"' + " joined the room."
+    };
 
-        // Tell everyone in the room that the new client joined
-        io.in(room).emit('chat message', data2);
-        console.log('[' + room + '] ' + data.name + ' joined');
-    });
+    // Tell everyone in the room that the new client joined
+    io.in(room).emit("chat message", data2);
+    console.log("[" + room + "] " + data.name + " joined");
+  });
 
-    // When a client wants to connect to a room
-    socket.on('connect to room', function(room) {
-        socket.join(room);
-    });
+  // When a client wants to connect to a room
+  socket.on("connect to room", function(room) {
+    socket.join(room);
+  });
 });
 
-http.listen(3000, function(){
-    console.log('wiphat started on 3000');
+http.listen(3000, function() {
+  console.log("wiphat started on 3000");
 });
