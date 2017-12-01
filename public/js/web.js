a3f979f209ead2c9694a76f38c32d0a44b18bb01
kaanklky <kaanklky@gmail.com>
2017-12-01 21:06:55 +0300

    Remove Join section, will join to room over a service

--- public/js/web.js
+++ public/js/web.js
@@ -13,3 +13,7 @@ function makeId(length, alpha) {
 
   return text;
 }
+
+function copyToClipboard(roomId) {
+  window.prompt("copy & send to your friend", "http://join.private.kaankolkoy.net/" + roomId);
+}
